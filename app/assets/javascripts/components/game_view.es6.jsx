7c8f160f3e65e9d0dace889421243b88b05c6d50
Austin Lanari <email": null>
2016-10-10 17:33:12 -0400

    begin to build initial card function

--- app/assets/javascripts/components/game_view.es6.jsx
+++ app/assets/javascripts/components/game_view.es6.jsx
@@ -1,29 +1,27 @@
 class GameView extends React.Component {
+  constructor() {
+    super();
+    this.initialCards = this.initialCards.bind(this);
+  }
+
+  initialCards() {
+    var firstCards = [];
+    for (var i=0; i<9; i++) {
+      var index = Math.floor(Math.random()*this.props.cards.length)
+      firstCards.push(this.props.cards[index])
+      this.props.cards.splice(index, 1)
+    }
+    return firstCards
+  }
   render() {
+    let { cards } = this.props.cards;
     return(
   <div className="row">
-    <div className="col-md-4">
-    </div>
-    <div className="col-md-4">
-    </div>
-    <div className="col-md-4">
-    </div>
-
-
-    <div className="col-md-4">
-    </div>
-    <div className="col-md-4">
-    </div>
-    <div className="col-md-4">
-    </div>
-
-
-    <div className="col-md-4">
-    </div>
-    <div className="col-md-4">
-    </div>
-    <div className="col-md-4">
-    </div>
+    {cards.map((card, i) =>
+      <div className="col-md-4">
+        <Card key={i} data={card} />
+      </div>
+      )}
   </div>
   <div className="row">
     <div className="col-md-4 col-md-offset-8">
