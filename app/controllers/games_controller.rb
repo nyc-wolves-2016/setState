d190e922d0f12f82cb8a2c534f4e174ecd558b7f
Austin Lanari <email": null>
2016-10-10 16:16:22 -0400

    Create wireframe and routes for game creation and show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,22 @@
 class GamesController < ApplicationController
+
   def new
     Game.all
   end
+
+  def create
+    @game = Game.new()
+    current_user.games << @game
+    if @game.save
+      redirect_to 'games/show'
+    else
+      render 'games/new'
+    end
+  end
+
+  def show
+    render 'games/show'
+  end
+
+
 end
