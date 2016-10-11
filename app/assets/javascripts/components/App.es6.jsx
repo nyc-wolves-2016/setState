40fd56913e45691697fc5da27e942a37a4f524d6
Austin Lanari & Neel Gupta <pair+foggy1.neelgupta2@devbootcamp.com>
2016-10-11 11:59:11 -0400

    Build set validator; psuedocode state hierarchy

--- app/assets/javascripts/components/App.es6.jsx
+++ app/assets/javascripts/components/App.es6.jsx
@@ -2,13 +2,31 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      cards = [];
+      currentBoard = [];
+      correctGuesses = [];
+      errors = [];
+      gameId = 1;
     };
+    // Defines method to pull data from GameView
+    this.handleThree = this.handleThree.bind(this);
+  }
+
+  handleThree(set) {
+    // set comes from onThree / handleThree
+    // Remove set from GameView (?)
+  }
+
+  handleError(error) {
+    // False set comes in from GameView
+    // Throw 'error' to top of GameView
+    // this.setState(prevState)
   }
 
   render() {
     return (
       <div className="appContainer">
-        
+        <GameView data={this.state.currentBoard, this.state.correctGuesses, this.state.gameId} addThree={} onThree={this.handleThree} onError={this.handleError} />
       </div>
     )
   }
